4b571855c96489978b9d10fb37494531f8f75cf8
webbdevv <kyle.xu4@gmail.com>
2021-11-08 13:43:26 -0800

    Pruning

--- src/util.js
+++
@@ -1,6 +0,0 @@
-async function getPosts(url = "https://worker_project.kyle-xu4.workers.dev/posts"){
-  let res = await fetch(url, {method: "GET", mode: "no-cors"});
-  return res.json();
-}
-
-export default getPosts;
